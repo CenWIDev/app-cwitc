84482b705e0ea33f1de37675707d92cd77dfc4c4
Drew Frisk <hello@drewfrisk.com>
2018-10-03 20:03:56 -0500

    some minor cleanup
    * fixed android renderers
    * android status bar color

--- src/CWITC.Android/MainActivity.cs
+++ src/CWITC.Android/MainActivity.cs
@@ -23,75 +23,70 @@ using CWITC.Clients.UI;
 using CWITC.DataObjects;
 using Xamarin;
 using Android.Gms.Auth.Api.SignIn;
-//using Microsoft.Azure.Mobile;
-//using Microsoft.Azure.Mobile.Crashes;
-//using Microsoft.Azure.Mobile.Analytics;
 using Android.Gms.Tasks;
 
 namespace CWITC.Droid
 {
 
 
-    [Activity(Label = "@string/app_name",
-        Name = "org.cenwidev.cwitc.MainActivity",
-        Exported = true,
-        Icon = "@drawable/ic_launcher",
-        LaunchMode = LaunchMode.SingleTask,
-        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity(Name = "org.cenwidev.cwitc.MainActivity",
+		Exported = true,
+		Icon = "@drawable/ic_launcher",
+		LaunchMode = LaunchMode.SingleTask,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+	    ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
 	[IntentFilter(new[] { Intent.ActionView },
 			Categories = new[] { Intent.CategoryDefault, Intent.CategoryBrowsable },
 			DataScheme = "org.cenwidev.cwitc",
 			DataHost = "cwitc.auth0.com",
 			DataPathPrefix = "/android/org.cenwidev.cwitc/callback")]
-    public class MainActivity : FormsAppCompatActivity
-    {
-        const int RC_SIGN_IN = 9001;
-        TaskCompletionSource<GoogleSignInAccount> googleSignInTask = null;
+	public class MainActivity : FormsAppCompatActivity
+	{
+		const int RC_SIGN_IN = 9001;
 
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			ToolbarResource = Resource.Layout.toolbar;
+			TabLayoutResource = Resource.Layout.tabs;
 
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            ToolbarResource = Resource.Layout.toolbar;
-            TabLayoutResource = Resource.Layout.tabs;
+			base.OnCreate(savedInstanceState);
 
-            base.OnCreate(savedInstanceState);
+			Forms.Init(this, savedInstanceState);
+			FormsMaps.Init(this, savedInstanceState);
+			Toolkit.Init();
 
-            Forms.Init(this, savedInstanceState);
-            FormsMaps.Init(this, savedInstanceState);
-            Toolkit.Init();
+			DependencyService.Register<IAuthClient, Auth0Client>();
 
-            DependencyService.Register<IAuthClient, Auth0Client>();
+			PullToRefreshLayoutRenderer.Init();
+			typeof(Color).GetProperty("Accent", BindingFlags.Public | BindingFlags.Static).SetValue(null, Color.FromHex("#757575"));
 
-            PullToRefreshLayoutRenderer.Init();
-            typeof(Color).GetProperty("Accent", BindingFlags.Public | BindingFlags.Static).SetValue(null, Color.FromHex("#757575"));
+			ImageCircle.Forms.Plugin.Droid.ImageCircleRenderer.Init();
 
-            ImageCircle.Forms.Plugin.Droid.ImageCircleRenderer.Init();
+			ZXing.Net.Mobile.Forms.Android.Platform.Init();
 
-            ZXing.Net.Mobile.Forms.Android.Platform.Init();
+			LoadApplication(new App());
 
-            LoadApplication(new App());
+			OnNewIntent(Intent);
 
-            OnNewIntent(Intent);
-
-            if (!string.IsNullOrWhiteSpace(Intent?.Data?.LastPathSegment))
-            {
-                switch (Intent.Data.LastPathSegment)
-                {
-                    case "sessions":
-                        MessagingService.Current.SendMessage<DeepLinkPage>("DeepLinkPage", new DeepLinkPage
-                        {
-                            Page = AppPage.Sessions
-                        });
-                        break;
-                    case "events":
-                        MessagingService.Current.SendMessage<DeepLinkPage>("DeepLinkPage", new DeepLinkPage
-                        {
-                            Page = AppPage.Schedule
-                        });
-                        break;
-                }
-            }
-        }
+			if (!string.IsNullOrWhiteSpace(Intent?.Data?.LastPathSegment))
+			{
+				switch (Intent.Data.LastPathSegment)
+				{
+					case "sessions":
+						MessagingService.Current.SendMessage<DeepLinkPage>("DeepLinkPage", new DeepLinkPage
+						{
+							Page = AppPage.Sessions
+						});
+						break;
+					case "events":
+						MessagingService.Current.SendMessage<DeepLinkPage>("DeepLinkPage", new DeepLinkPage
+						{
+							Page = AppPage.Schedule
+						});
+						break;
+				}
+			}
+		}
 
 		protected override void OnNewIntent(Intent intent)
 		{
@@ -100,10 +95,10 @@ namespace CWITC.Droid
 			Auth0.OidcClient.ActivityMediator.Instance.Send(intent.DataString);
 		}
 
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
-        {
-            global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+		{
+			global::ZXing.Net.Mobile.Forms.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+			PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
 }
---
+++ src/CWITC.Android/Renderers/PageRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(CWITC.Droid.PageRenderer))]
+namespace CWITC.Droid
+{
+	public class PageRenderer : Xamarin.Forms.Platform.Android.PageRenderer
+	{
+		public PageRenderer(Android.Content.Context context) : base(context)
+		{
+			
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<Page> e)
+		{
+			base.OnElementChanged(e);
+
+			if (e.NewElement != null)
+			{
+				this.Element.ForceLayout();
+			}
+		}
+	}
+}
