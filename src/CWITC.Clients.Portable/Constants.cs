fd2acd3039a49cf2050b0c3a4101bf762a86b4ef
Drew Frisk <455639+MysterDru@users.noreply.github.com>
2019-09-08 19:54:09 -0500

    fixing more build errors for app center (#74)
    * fix android project build errors
    
    * fixing more build errors

--- src/CWITC.Clients.Portable/Constants.cs
+++ src/CWITC.Clients.Portable/Constants.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 namespace CWITC.Clients.Portable
 {
-    public static class Constants
-    {
-		public const string FacebookAppId = "";
+	public static class Constants
+	{
+		public const string FacebookAppId = "1391179434308637";
 
-		public const string GithubClientId = "";
-		public const string GithubClientSecret = "";
+		public const string GithubClientId = "40c527d407bf23a53b88";
+		public const string GithubClientSecret = "3596d1579cc2cf82c892ad9e1b17fede60d9233a";
 
-		public const string TwitterClientId = "";
-		public const string TwitterClientSecret = "";
+		public const string TwitterClientId = "DOirvDQts9ncdphpKxH7PT8X1";
+		public const string TwitterClientSecret = "gUecmloB7soEsjwMNagxrKJO9nTYx7jf51FDBlKJIn63hnPyE9";
 	}
 }
