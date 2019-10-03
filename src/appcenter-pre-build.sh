23f9b38c0f587aebdcb2f17e485eaafb5afefaa2
Drew Frisk <455639+MysterDru@users.noreply.github.com>
2019-10-02 21:24:51 -0500

    fix build script typo (#89)

--- src/appcenter-pre-build.sh
+++ src/appcenter-pre-build.sh
@@ -46,4 +46,4 @@ then
 fi
 
 echo "{\"FacebookAppId\": \""$FACEBOOK_APPID"\",\"GithubClientId\": \""$GITHUB_CLIENTID"\",\"GithubClientSecret\": \""$GITHUB_CLIENTSECRET"\",\"TwitterClientId\": \""$TWITTER_CLIENTID"\",\"TwitterClientSecret\": \""$TWITTER_CLIENTSECRET"\",\"ContentfulSpaceKey\": \""$CONTENTFUL_SPACEKEY"\",\"ContentfulDeliveryApiKey\": \""$CONTENTFUL_DELIVERYAPIKEY"\"}" > cp Config.json
-cp Config.json $APPCENTER_SOURCE_DIRECTORY'/CWITC.Clients.Portable/Config.json"
+cp Config.json $APPCENTER_SOURCE_DIRECTORY"/CWITC.Clients.Portable/Config.json"
