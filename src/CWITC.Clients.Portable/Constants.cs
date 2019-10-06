925e60d15dfa0ab09a37dc7ed6a4b5fcfc107e28
Drew Frisk <hello@drewfrisk.com>
2019-10-05 19:07:53 -0500

    got session feedback working
    fixed issues with build secrets

---
+++ src/CWITC.Clients.Portable/Constants.cs
@@ -0,0 +1,12 @@
+using System;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("CWITC.Droid")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("CWITC.Clients.UI")]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("CWITCiOS")]
+
+namespace CWITC.Clients.Portable
+{
+	public class Constants
+	{
+	}
+}
--- src/CWITC.Shared/Datastore/FeedbackStore.cs
+++ src/CWITC.Shared/Datastore/FeedbackStore.cs
@@ -8,6 +8,7 @@ using CWITC.DataStore.Abstractions;
 using CWITC.Shared.DataStore;
 using Newtonsoft.Json.Linq;
 using Xamarin.Forms;
+using Newtonsoft.Json;
 #if __IOS__
 using Foundation;
 #elif __ANDROID__
@@ -37,55 +38,61 @@ namespace CWITC.Shared.DataStore
 
 			return dict.Values.ToList();
 		}
-
 #if __IOS__
 		protected override NSDictionary GetSaveValue(IEnumerable<Feedback> existingItems)
 		{
 			var dict = GetSaveDictionary(existingItems);
+			var json = JsonConvert.SerializeObject(dict);
 
-			var result = NSDictionary
-				.FromObjectsAndKeys(dict.Values.ToArray(), dict.Keys.ToArray());
+			var data = NSData.FromString(json);
+			var result = NSJsonSerialization.Deserialize(data, NSJsonReadingOptions.AllowFragments, out NSError readError) as NSDictionary;
 
 			return result;
 		}
+
 #elif __ANDROID__
 		protected override Java.Lang.Object GetSaveValue(IEnumerable<Feedback> existingItems)
 		{
 			var dict = GetSaveDictionary(existingItems);
+
 			var map = new HashMap();
 
 			foreach (var key in dict.Keys)
 			{
-				map.Put(new Java.Lang.String(key), new FeedbackJavaObj
-				{
-					ContentfulId = dict[key].ContentfulId,
-					FeedbackText = dict[key].FeedbackText,
-					SessionRating = dict[key].SessionRating
-				});
+				var item = new HashMap();
+				item.Put(new Java.Lang.String("contentfulId"), new Java.Lang.String(dict[key].ContentfulId));
+				item.Put(new Java.Lang.String("feedbackText"), new Java.Lang.String(dict[key].FeedbackText));
+				item.Put(new Java.Lang.String("sessionRating"), new Java.Lang.Double(dict[key].SessionRating));
+
+				map.Put(key, item);
 			}
 
 			return map;
-		}
-
-		class FeedbackJavaObj : Java.Lang.Object
-		{
-			public string ContentfulId { get; set; }
-
-			public string FeedbackText { get; set; }
-
-			public double SessionRating { get; set; }
-		}
+		}		
 #endif
 
 		Dictionary<string, Feedback> GetSaveDictionary(IEnumerable<Feedback> items)
 		{
-			Dictionary<string, Feedback> tosave = new Dictionary<string, Feedback>();
+			var tosave = new Dictionary<string, Feedback>();
 			foreach (var item in items)
 			{
 				tosave.Add(item.ContentfulId, item);
 			}
 
 			return tosave;
+		}
+
+#if __ANDROID__
+		class FeedbackJavaObj : Java.Lang.Object
+#elif __IOS__
+		class FeedbackNSObject : NSObject
+#endif
+		{
+			public string ContentfulId { get; set; }
+
+			public string FeedbackText { get; set; }
+
+			public double SessionRating { get; set; }
 		}
 	}
 }
